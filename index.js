e493e3f0780d4e4e2ec90575ebda48ddfb10f5b7
majesty-01 <moheethasan4@gmail.com>
2023-05-19 23:31:20 +0600

    added single toy route for toy details page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,6 +29,14 @@ async function run() {
 
     app.get("/allToys", async (req, res) => {
       const result = await toyCollection.find().limit(20).toArray();
+      res.send(result);
+    });
+
+    app.get("/allToys/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
       res.send(result);
     });
 
